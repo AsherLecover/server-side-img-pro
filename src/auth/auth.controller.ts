dbe7bf350af1e4ec1ba7b4a214e93797b82404b1
AsherLecover <asherlec5@gmail.com>
2020-11-23 19:09:48 +0200

    initail commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,35 +1,55 @@
-import { Body, Controller, Get, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
-import { AuthCredentialsDto, AuthCredentialsDtoSignin } from './dto/auth-credentials.dto';
+import {
+  AuthCredentialsDto,
+  AuthCredentialsDtoSignin,
+} from './dto/auth-credentials.dto';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-    signUp(
-        @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto):Promise<void>{
-        console.log('authCredentialsDto: (from controller..)',authCredentialsDto);
-        return this.authService.signUp(authCredentialsDto)
-    }
+  signUp(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
+    console.log('authCredentialsDto: (from controller..)', authCredentialsDto);
+    return this.authService.signUp(authCredentialsDto);
+  }
 
-    @Post('/signin')
-    // @UseGuards(AuthGuard())
-    signIn(@Body(ValidationPipe) authCredentialsDtoSignin: AuthCredentialsDtoSignin): Promise< {accessToken: string}>{
-       
-        return this.authService.signIn(authCredentialsDtoSignin);
-    }
+  @Post('/signin')
+  // @UseGuards(AuthGuard())
+  signIn(
+    @Body(ValidationPipe) authCredentialsDtoSignin: AuthCredentialsDtoSignin,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDtoSignin);
+  }
 
-    // @UseGuards(AuthGuard('facebook-token'))
-    @Get('/facebook')
-    async getTokenAfterFacebookSignIn(@Req() req) {
-      console.log(req);
-    //   return this.authService.findOrCreate(req)
-    }
-    //auth/facebook
+  @Get('facebook')
+  @UseGuards(AuthGuard('facebook'))
+  async facebookLogin(): Promise<any> {
+    return HttpStatus.OK;
+  }
 
-   
+  @Get('facebook/redirect')
+  @UseGuards(AuthGuard('facebook'))
+  async facebookLoginRedirect(@Req() req: Request): Promise<any> {
+    console.log(req.user);
 
-    
+    return {
+      statusCode: HttpStatus.OK,
+      payload: req.user,
+    };
+  }
 }
--- src/auth/facebook.stategy.ts
+++ src/auth/facebook.stategy.ts
@@ -1,30 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import * as FacebookTokenStrategy from 'passport-facebook-token';
-import { use } from 'passport';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-facebook';
 
 @Injectable()
-export class FacebookStrategy {
+export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
   constructor() {
-    this.init();
+    super({
+      clientID: '718817949052988',
+      clientSecret: 'fd17b7ac4fa40e00d9b825057a01d1c6',
+      callbackURL: 'http://localhost:3000/auth/facebook/redirect',
+      scope: 'email',
+      profileFields: ['emails', 'name','photos'],
+    });
   }
-  init() {
-    use(
-      new FacebookTokenStrategy(
-        {
-          clientID: '718817949052988',
-          clientSecret: 'fd17b7ac4fa40e00d9b825057a01d1c6',
-          
-        },
-        async (
-          accessToken: string,
-          refreshToken: string,
-          profile: any,
-          done: any,
-        ) => {
-          const user = {};
-          return done(null, user);
-        },
-      ),
-    );
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (err: any, user: any, info?: any) => void,
+  ): Promise<any> {
+    const { name, emails,photos } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+      photos: photos[0].value
+
+    };
+    const payload = {
+      user,
+      accessToken,
+    };
+
+    done(null, payload);
   }
 }
