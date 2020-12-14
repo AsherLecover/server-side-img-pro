e38ed04b4ab71c893935b5c005f15a32a77ee105
Asher Lecover <Asherlec5@gmail.con>
2020-12-15 01:01:05 +0200

    handlinig chat between users

--- src/chat-messages/chat-messages.gateway.ts
+++ src/chat-messages/chat-messages.gateway.ts
@@ -1,4 +1,5 @@
 import { SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
+import { text } from 'express';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
@@ -7,8 +8,10 @@ export class ChatMessagesGateway {
 @WebSocketServer() wss:Server
 
   @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, paylowd: {senderId: number,  reciverId:number, text: string, time: Date}):void {
+  handleMessage(client: Socket, paylowd: {senderId: number, reciverId:number, text: string, time: Date}):void {
     console.log(12453524, paylowd);
+    // client.broadcast.to()
+    // this.wss.to(paylowd.reciverId.toString()).emit('msgToClinet',paylowd) 
     this.wss.emit('msgToClinet',paylowd)
 
   }
