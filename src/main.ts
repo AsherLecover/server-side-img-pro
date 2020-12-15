3a9b782f44c267414122d7f102d8695f3bc50c6c
AsherLecover <asherlec5@gmail.com>
2020-12-15 13:58:42 +0200

    make it better

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors:true});
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
 }
